f4f5378f84dbd8b0ec20c899db3d4061f2b23d2e
Gagogaa <gregoryjmann26@gmail.com>
2016-09-15 10:54:01 -0400

    test

--- Lab-1/lab1.java
+++ Lab-1/lab1.java
@@ -1,2 +1,37 @@
+/*
+	Option one convert dec to bin
+	Option two convert bin to dec
+	Option three is quit
+	
+	NOTE: print out an array for option one
+*/
+import java.util.Scanner;
+
 public class Lab1 {
+	public static void main(String[] args) {
+		
+		while(true) {
+			
+		}
+	}
+	
+	public static String decToBin(int number){
+		
+		return "";
+	}
+	public static int[] convertUInt(int num){
+		int[] x = new int[8];
+		for(int i = 0; i <=8; i++){
+			x[i] = hasRemainder(num);
+			num = num / 2;
+		}
+		return x;
+	}
+	public static int hasRemainder(int num){
+		if(num % 2 > 0){
+			return 1;
+		}else{
+			return 0;
+		}
+	}
 }
